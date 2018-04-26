484225c99e7514a9d185d12f5cf085c495837a55
gautamV <gautam.venugopalan@ligo.org>
2018-04-26 14:00:27 -0700

    making spectrogram plotting func

--- python_40mutils/generalUtils.py
+++ python_40mutils/generalUtils.py
@@ -3,6 +3,8 @@ Collection of generally useful functions for making ELOG plots etc
 '''
 import sys, os, subprocess
 import numpy as np
+import scipy.signal as sig
+import nds2
 
 def tarballz(outFile, fileList):
 	'''
@@ -18,4 +20,19 @@ def tarballz(outFile, fileList):
 	subprocess.call(tarCommand, shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
 	return
 
-
+def makeLSCspecgram(channel, tStart, tStop, t_fft=4, win=('tukey', 0.25)):
+	'''
+	Computes a spectrogram by downloading data from NDS2.
+	Example usage:
+		tt, ff, Sxx = makeLSCspecgram(['C1:LSC-XARM_IN1_DQ'],1208808916,1208808917, t_fft=4, win=('tukey', 0.25))
+	will download the data, and make a spectrogram with 4 second FFT time.
+	Option to specify windowing function used by scipy.signal.spectrogram.
+	Note that Power Spectral Density is returned, use sqrt where appropriate!
+	'''
+	conn = nds2.connection('nds40.ligo.caltech.edu', 31200)
+	print('Getting NDS data')
+	dat = conn.fetch(tStart, tStop, channel)
+	print('Got NDS data, now making plot')
+	Fs = dat[0].channel.sample_rate
+	ff, tt, Sxx = sig.spectrogram(dat[0].data,fs=Fs,nperseg=int(t_fft*Fs),window=win)
+	return ff, tt, Sxx

fe09ccf22e3312253ef5a44771ff9b2d58466434
gautamV <gautam.venugopalan@ligo.org>
2018-04-27 17:25:39 -0700

    more updates to generalUtils

--- python_40mutils/generalUtils.py
+++ python_40mutils/generalUtils.py
@@ -36,3 +36,28 @@ def makeLSCspecgram(channel, tStart, tStop, t_fft=4, win=('tukey', 0.25)):
 	Fs = dat[0].channel.sample_rate
 	ff, tt, Sxx = sig.spectrogram(dat[0].data,fs=Fs,nperseg=int(t_fft*Fs),window=win)
 	return ff, tt, Sxx
+
+def makeASD(channel, tStart, tStop, t_fft=4, win=('tukey', 0.25)):
+	'''
+	Computes an ASD by downloading data from NDS2 using pwelch.
+	Example usage:
+		ff, Pxx = makeASD(['C1:LSC-XARM_IN1_DQ'],1208808916,1208808917, t_fft=4, win=('tukey', 0.25))
+	will download the data, and make a ASD with 4 second FFT time.
+	Option to specify windowing function used by scipy.signal.welch
+	Note that Power Spectral Density is returned, use sqrt where appropriate!
+	'''
+	conn = nds2.connection('nds40.ligo.caltech.edu', 31200)
+	print('Getting NDS data')
+	dat = conn.fetch(tStart, tStop, channel)
+	print('Got NDS data...')
+	Fs = dat[0].channel.sample_rate
+	ff, Pxx = sig.welch(dat[0].data,fs=Fs,nperseg=int(t_fft*Fs),window=win)
+	return ff, Pxx
+
+def cum_rms(ff, asd): 
+	df = np.roll(ff, -1) - ff
+	df = df[:-1]
+	cum_ms = np.zeros(len(ff))
+	for ii in range(len(cum_ms)):
+		cum_ms[ii] = np.trapz(asd[ii:]**2, ff[ii:])
+	return cum_ms**0.5

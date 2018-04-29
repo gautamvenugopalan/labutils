949ac26ff5e41616562b0546e191402f864ec58d
Gautam Venugopalan <gautam@caltech.edu>
2018-04-28 23:19:29 -0700

    some more utils...

--- python_40mutils/generalUtils.py
+++ python_40mutils/generalUtils.py
@@ -61,3 +61,46 @@ def cum_rms(ff, asd):
 	for ii in range(len(cum_ms)):
 		cum_ms[ii] = np.trapz(asd[ii:]**2, ff[ii:])
 	return cum_ms**0.5
+
+def plotSpec(ff,ax,col=[1],doFormat=False,xlabel='Frequency [Hz]',ylabel='$\mathrm{V_{rms}}/\sqrt{\mathrm{Hz}}$',**kwargs):
+	'''
+	Function for plotting a spectrum from SR785 or AG4395.
+	Expects column 0 to be frequency, column 1/2 to be Vrms/rtHz.
+	Plotting arguments, e.g. color, are passed as **kwargs
+	'''
+	dat = np.loadtxt(ff)
+	for ii in col:
+		ax.loglog(dat[:,0],dat[:,int(ii)],**kwargs)
+	if doFormat:
+		ax.set_xlabel(xlabel)
+		ax.set_ylabel(ylabel)
+		ax.grid('on', which='both', linestyle='--',alpha=0.4)
+	return
+
+def plotTF(ff,axMag, axPh, cols='dbdeg',doFormat=False,magLabel='',**kwargs):
+	'''
+	Function for plotting a transfer function from SR785 or AG4395 or modeling.
+	Input has to be a 3 column file, with first column frequency.
+	Plotting arguments, e.g. color, are passed as **kwargs
+	'''
+	dat = np.loadtxt(ff)
+	if cols=='dbdeg':
+		axMag.semilogx(dat[:,0],dat[:,1],**kwargs)
+		axPh.semilogx(dat[:,0], dat[:,2],**kwargs)
+	elif cols=='absdeg':
+		axMag.loglog(dat[:,0],dat[:,1],**kwargs)
+	elif cols=='reim':
+		cplx = dat[:,1] + 1j*dat[:,2]
+		axMag.semilogx(dat[:,0],20*np.log10(np.abs(cplx)),**kwargs)
+		axPh.semilogx(dat[:,0],np.rad2deg(np.angle(cplx)))
+	else:
+		print('Unrecognized file formatting. Please use either dbdeg, absdeg or reim')
+		return
+	if doFormat:
+		axMag.set_ylabel(magLabel)
+		axPh.set_ylabel('Phase [degrees]')
+		axPh.set_xlabel('Frequency [Hz]')
+		axPh.set_yticks(np.linspace(-180,180,10))
+		axMag.grid('on',which='both',linestyle='--',alpha=0.4)
+		axPh.grid('on',which='both',linestyle='--',alpha=0.4)
+	return

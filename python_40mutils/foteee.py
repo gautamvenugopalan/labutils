803db3de75c53fa48ad2b5054e37553d9bee70be
Gautam Venugopalan <gautam@caltech.edu>
2020-05-15 15:05:33 -0700

    Adds functionality for computing pole frequencies  - DARM pole for ESR  - DARM pole for RSE  - DARM pole for PRFPMI

--- python_40mutils/foteee.py
+++ python_40mutils/foteee.py
@@ -5,6 +5,7 @@ Set of python utilities for the 40m interferometer
 import generalUtils as genUtils
 import scipy.signal as sig
 import numpy as np
+import scipy.constants as scc
 
 def AAfilt(ff, f0_z=1e3*np.array([13.5305051680, 15.5318357741, 23.1746351749]),
         Q_z=np.array([423.6130434049e6, 747.6895990654e3, 1.5412966100e6]),
@@ -81,3 +82,112 @@ def AIfilt(ff, f0_z=1e3*np.array([16.5683962778, 32.6135551736]),
     k *= np.abs(np.prod(poles)/np.prod(zeros))
     ww, hh = sig.freqs_zpk(zeros, poles, k, 2*np.pi*ff)
     return(ww, hh)
+
+def DARMpole_RSE(Ts=9.903e-2, Ti=1.384e-2, Te=13.7e-6, Eta_arm=50e-6, Eta_SRM=1000e-6, Larm=37.795):
+    '''
+    Function that calculates the RSE pole for the 40m, under
+    the single-pole approximation for cavity transfer functions.
+
+    Parameters:
+    ------------
+    Ts: float
+        SRM power transmissivity. Defaults to 9.903 %.
+    Ti: float
+        ITM power transmissivity, Defaults to 1.384 %.
+    Te: float
+        ETM power transmissivity. Defaults to 13.7 ppm.
+    Eta_arm: float
+        Arm cavity loss. Split equally between ITM and ETM.
+        Defaults to 50 ppm, round trip.
+    Eta_SRM: float
+        SRC cavity scatter loss. Defaults to 1000ppm.
+    Larm: float
+        Length of arm cavity [m]. Defaults to 37.795 m.
+
+    Returns:
+    -----------
+    fP: float
+        DARM pole [Hz], assuming the SRC is configured for RSE.
+    '''
+    ts = np.sqrt(Ts)
+    rs = np.sqrt(1 - Ts - Eta_SRM)
+    ti = np.sqrt(Ti)
+    ri = np.sqrt(1 - Ti - Eta_arm/2)
+    te = np.sqrt(Te)
+    re = np.sqrt(1 - Te - Eta_arm/2)
+    num = 1 - ri*rs
+    den = re*ri - re*rs*(ti**2 + ri**2)
+    fP = scc.c / 2 / Larm 
+    fP *= np.log(num/den)
+    return(fP/2/np.pi)
+
+def DARMpole_ESR(Ts=9.903e-2, Ti=1.384e-2, Te=13.7e-6, Eta_arm=50e-6, Eta_SRM=1000e-6, Larm=37.795):
+    '''
+    Function that calculates the ESR pole for the 40m, under
+    the single-pole approximation for cavity transfer functions.
+
+    Parameters:
+    ------------
+    Ts: float
+        SRM power transmissivity. Defaults to 9.903 %.
+    Ti: float
+        ITM power transmissivity, Defaults to 1.384 %.
+    Te: float
+        ETM power transmissivity. Defaults to 13.7 ppm.
+    Eta_arm: float
+        Arm cavity loss. Split equally between ITM and ETM.
+        Defaults to 50 ppm, round trip.
+    Eta_SRM: float
+        SRC cavity scatter loss. Defaults to 1000ppm.
+    Larm: float
+        Length of arm cavity [m]. Defaults to 37.795 m.
+
+    Returns:
+    -----------
+    fP: float
+        DARM pole [Hz], assuming the SRC is configured for ESR.
+    '''
+    ts = np.sqrt(Ts)
+    rs = np.sqrt(1 - Ts - Eta_SRM)
+    ti = np.sqrt(Ti)
+    ri = np.sqrt(1 - Ti - Eta_arm/2)
+    te = np.sqrt(Te)
+    re = np.sqrt(1 - Te - Eta_arm/2)
+    num = 1 + ri*rs
+    den = re*ri + re*rs*(ti**2 + ri**2)
+    fP = scc.c / 2 / Larm 
+    fP *= np.log(num/den)
+    return(fP/2/np.pi)
+
+
+def DARMpole_PRFPMI(Ti=1.384e-2, Te=13.7e-6, Eta_arm=50e-6, Larm=37.795):
+    '''
+    Function that calculates the DARM pole for the 40m configured as a PRFPMI, 
+    under the single-pole approximation for cavity transfer functions.
+
+    Parameters:
+    ------------
+    Ti: float
+        ITM power transmissivity, Defaults to 1.384 %.
+    Te: float
+        ETM power transmissivity. Defaults to 13.7 ppm.
+    Eta_arm: float
+        Arm cavity loss. Split equally between ITM and ETM.
+        Defaults to 50 ppm, round trip.
+    Larm: float
+        Length of arm cavity [m]. Defaults to 37.795 m.
+
+    Returns:
+    -----------
+    fP: float
+        DARM pole [Hz], assuming a PRFPMI.
+    '''
+    ti = np.sqrt(Ti)
+    ri = np.sqrt(1 - Ti - Eta_arm/2)
+    te = np.sqrt(Te)
+    re = np.sqrt(1 - Te - Eta_arm/2)
+    num = 1
+    den = re*ri
+    fP = scc.c / 2 / Larm 
+    fP *= np.log(num/den)
+    return(fP/2/np.pi)

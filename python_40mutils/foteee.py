4d77203a59156c5c383cb1bca70339b63d1e7d7d
Gautam Venugopalan <gautam@caltech.edu>
2020-05-15 21:01:17 -0700

    Adds more functionality for calculating DARM loop  - Function for calculating TF of DAC included.  - Function for calculating TF of DIGITAL AA/AI filters included  - Function for evaluating the CARM pole frequency added

--- python_40mutils/foteee.py
+++ python_40mutils/foteee.py
@@ -191,3 +191,93 @@ def DARMpole_PRFPMI(Ti=1.384e-2, Te=13.7e-6, Eta_arm=50e-6, Larm=37.795):
     fP = scc.c / 2 / Larm 
     fP *= np.log(num/den)
     return(fP/2/np.pi)
+
+def CARMpole(Ti=1.384e-2, Te=13.7e-6, Tp=5.637e-2, Eta_arm=50e-6, Eta_PRC=0.5e-2, Larm=37.795):
+    '''
+    Function that calculates the CARM pole for the 40m.
+
+    Parameters:
+    ------------
+    Ti: float
+        ITM power transmissivity, Defaults to 1.384 %.
+    Te: float
+        ETM power transmissivity. Defaults to 13.7 ppm.
+    Tp: float
+        PRM power transmissivity. Defaults to 5.637 %.
+    Eta_arm: float
+        Arm cavity loss. Split equally between ITM and ETM.
+        Defaults to 50 ppm, round trip.
+    Eta_PRC: float
+        Loss in the power recycling cavity, including mode
+        mismatch. Defaults to 0.5%.
+    Larm: float
+        Length of arm cavity [m]. Defaults to 37.795 m.
+
+    Returns:
+    --------
+    fP: float
+        CARM pole [Hz].
+    '''
+    tp = np.sqrt(Tp)
+    rp = np.sqrt(1 - Tp - Eta_PRC)
+    ti = np.sqrt(Ti)
+    ri = np.sqrt(1 - Ti - Eta_arm/2)
+    te = np.sqrt(Te)
+    re = np.sqrt(1 - Te - Eta_arm/2)
+    num = 1 + ri*rp
+    den = re*ri + re*rp*(ti**2 + ri**2)
+    fP = np.log(num/den) * scc.c / 2 / Larm / 2 / np.pi
+    return(fP)
+
+def DAIfilt(ff, sosCoeffs=np.array([[1,-1.41346289716898, 0.99893884152400, 
+    1, -1.71662585474518, 0.78495484219691],
+    [1, 0.00000127375260, 0.99819981588176, 1, -1.68385964238855, 0.93734519457266]]), 
+    overallGain=0.014805052402446, fs=2*np.pi*2**16):
+    '''
+    Calculate the frequency response of the CDS Digital anti-aliasing filter.
+
+    Parameters:
+    ------------
+    ff: array_like
+        Frequency vector on which to evaluate
+    sosCoeffs: array_like
+        Coefficients describing the digital AI filter.
+        Defaults to what is found in 40m elog 3961.
+    overallGain: float
+        Overall gain of the digital AI filter.
+        Defaults to what is found in 40m elog 3961.
+    fs: int
+        Sampling frequency [Hz]. Defaults to 2**16 Hz.
+
+    Returns:
+    --------
+    ww: array_like
+        Vector of angular frequencies [rad/sec] on which response is evaluated.
+    hh: array_like
+        Response
+    '''
+    ww, hh = sig.sosfreqz(sosCoeffs, worN=2*np.pi*ff, whole=True, fs=fs)
+    return(ww,hh*overallGain)
+
+def DACTF(ff, fs=2**16):
+    '''
+    DAC transfer function in going from discrete time to
+    a voltage when sampled at fs.
+
+    Parameters:
+    -----------
+    ff: array_like
+        Frequency vector on which to evaluate the response
+    fs: int
+        Sampling frequency [Hz]. Defaults to 64 kHz.
+
+    Returns:
+    ---------
+    D: array_like
+        The transfer function from cts to V.
+    '''
+    x = np.pi*ff/fs
+    D = np.exp(-1j*x)
+    D *= np.sin(x)/x
+    return(D)
+
